962256cd5f73fcd0cb9332e783ea671ae59d0a05
Rahul Raj <rahulfrndz02@gmail.com>
2023-01-27 01:15:50 +0530

    final

--- src/main/java/com/driver/models/Transaction.java
+++ src/main/java/com/driver/models/Transaction.java
@@ -39,19 +39,6 @@ public class Transaction {
     @CreationTimestamp
     private Date transactionDate;
 
-    public Transaction() {
-    }
-
-    public Transaction(String transactionId, Card card, Book book, int fineAmount, boolean isIssueOperation, TransactionStatus transactionStatus, Date transactionDate) {
-        this.transactionId = transactionId;
-        this.card = card;
-        this.book = book;
-        this.fineAmount = fineAmount;
-        this.isIssueOperation = isIssueOperation;
-        this.transactionStatus = transactionStatus;
-        this.transactionDate = transactionDate;
-    }
-
     public int getId() {
         return id;
     }
